2c5b01fafd10297b556d35e694fd63aecf738715
yashkumarsingh995 <yashyuvraj9020@gmail.com>
2023-01-18 22:25:29 +0530

    updating the banner and some other changes like nav and spacing etc

---
+++ js/nav_anime.js
@@ -0,0 +1,26 @@
+document.getElementById("mainmenu_it").addEventListener("mouseover",()=>{
+    var elemet = document.getElementById('element_it');
+    elemet.style.display="block";
+}) ;
+document.getElementById("mainmenu_it").addEventListener("mouseleave",()=>{
+    var elemet = document.getElementById('element_it');
+    elemet.style.display="none";
+}) ;
+
+document.getElementById("mainmenu_bpo").addEventListener("mouseover",()=>{
+    var elemet = document.getElementById('element_bpo');
+    elemet.style.display="block";
+}) ;
+document.getElementById("mainmenu_bpo").addEventListener("mouseleave",()=>{
+    var elemet = document.getElementById('element_bpo');
+    elemet.style.display="none";
+}) ;
+
+document.getElementById("mainmenu_mar").addEventListener("mouseover",()=>{
+    var elemet = document.getElementById('element_market');
+    elemet.style.display="block";
+}) ;
+document.getElementById("mainmenu_mar").addEventListener("mouseleave",()=>{
+    var elemet = document.getElementById('element_market');
+    elemet.style.display="none";
+}) ;
